18bc2f923e2582d4dc6cee735c9e0d2956fd2394
Up <upcraftedlp@gmail.com>
2024-11-03 20:37:15 +0100

    make BlockItemProvider return BlockItem

--- Common/src/main/java/dev/upcraft/sparkweave/api/registry/block/BlockItemProvider.java
+++ Common/src/main/java/dev/upcraft/sparkweave/api/registry/block/BlockItemProvider.java
@@ -1,10 +1,16 @@
 package dev.upcraft.sparkweave.api.registry.block;
 
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public interface BlockItemProvider {
 
-    default Item createItem() {
-        return new Item(new Item.Properties());
-    }
+	default Item createItem() {
+		if (this instanceof Block block) {
+			return new BlockItem(block, new Item.Properties());
+		}
+
+		throw new IllegalStateException("BlockItemProvider implemented on non-block!");
+	}
 }
