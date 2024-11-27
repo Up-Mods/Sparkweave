410d4448bcff3bb021a7c6e0c0b9b8e8ddeadeff
Up <upcraftedlp@gmail.com>
2024-11-27 20:50:55 +0100

    small code cleanup

--- Fabric/src/main/java/dev/upcraft/sparkweave/fabric/service/FabricRegistryService.java
+++ Fabric/src/main/java/dev/upcraft/sparkweave/fabric/service/FabricRegistryService.java
@@ -6,10 +6,10 @@ import dev.upcraft.sparkweave.api.registry.RegistryHandler;
 import dev.upcraft.sparkweave.api.registry.RegistryVisitor;
 import dev.upcraft.sparkweave.fabric.impl.registry.FabricRegistryHandler;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class FabricRegistryService implements RegistryService {
@@ -28,7 +28,7 @@ public class FabricRegistryService implements RegistryService {
 	public <T> void visitRegistry(Registry<T> registry, RegistryVisitor<T> callback) {
 		RegistryEntryAddedCallback.event(registry).register((rawId, id, object) -> callback.accept(id, object));
 		// need to create a copy because the callback itself may add entries to the registry while we are iterating it
-		var copy = registry.entrySet().stream().collect(Collectors.toMap(e -> e.getKey().location(), Map.Entry::getValue));
+		var copy = registry.holders().collect(Collectors.toMap(holder -> holder.key().location(), Holder.Reference::value));
 		copy.forEach(callback);
 	}
 
