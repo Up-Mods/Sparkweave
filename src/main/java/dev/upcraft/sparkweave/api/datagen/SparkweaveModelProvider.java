456884a098eefc942affc58d03c622dd7a17f5ef
Up <upcraftedlp@gmail.com>
2025-02-04 13:12:32 +0100

    add custom model provider

--- src/main/java/dev/upcraft/sparkweave/api/datagen/SparkweaveModelProvider.java
+++ src/main/java/dev/upcraft/sparkweave/api/datagen/SparkweaveModelProvider.java
@@ -1,6 +1,6 @@
-package dev.upcraft.sparkweave.mixin.ext.datagen.fabric;
+package dev.upcraft.sparkweave.api.datagen;
 
-import dev.upcraft.sparkweave.api.util.ext.datagen.fabric.FabricModelProviderExt;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.models.BlockModelGenerators;
 import net.minecraft.data.models.blockstates.MultiVariantGenerator;
@@ -8,13 +8,13 @@ import net.minecraft.data.models.blockstates.Variant;
 import net.minecraft.data.models.blockstates.VariantProperties;
 import net.minecraft.data.models.model.ModelLocationUtils;
 import net.minecraft.world.level.block.Block;
-import org.spongepowered.asm.mixin.Mixin;
 
-@SuppressWarnings("AddedMixinMembersNamePattern")
-@Mixin(FabricModelProvider.class)
-public abstract class FabricModelProviderMixin implements FabricModelProviderExt {
+public abstract class SparkweaveModelProvider extends FabricModelProvider {
 
-	@Override
+	public SparkweaveModelProvider(FabricDataOutput output) {
+		super(output);
+	}
+
 	public void copyModelNoItem(BlockModelGenerators gen, Block sourceBlock, Block targetBlock) {
 		gen.blockStateOutput.accept(MultiVariantGenerator.multiVariant(targetBlock, Variant.variant().with(VariantProperties.MODEL, ModelLocationUtils.getModelLocation(sourceBlock))));
 	}
