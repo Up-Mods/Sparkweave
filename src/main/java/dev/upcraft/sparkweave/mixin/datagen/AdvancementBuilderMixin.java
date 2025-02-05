fae9c39b4ba3eef0fe186fc3ddf59e699e07883e
Up <upcraftedlp@gmail.com>
2025-02-05 17:56:00 +0100

    better advancement fix

--- src/main/java/dev/upcraft/sparkweave/mixin/datagen/AdvancementBuilderMixin.java
+++ src/main/java/dev/upcraft/sparkweave/mixin/datagen/AdvancementBuilderMixin.java
@@ -1,34 +1,28 @@
 package dev.upcraft.sparkweave.mixin.datagen;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import dev.upcraft.sparkweave.util.SparkweaveLogging;
 import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementRewards;
+import net.minecraft.advancements.RequirementsStrategy;
 import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.function.Function;
+import java.util.Map;
+import java.util.Set;
 
 @Mixin(Advancement.Builder.class)
 public class AdvancementBuilderMixin {
 
-	@Shadow
-	@Nullable
-	private ResourceLocation parentId;
-
-	@WrapOperation(method = "build", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/Advancement$Builder;canBuild(Ljava/util/function/Function;)Z"))
-	private boolean canActuallyBuild(Advancement.Builder instance, Function<ResourceLocation, Advancement> parentLookup, Operation<Boolean> original, ResourceLocation advancementId) {
-		boolean canBuild = original.call(instance, parentLookup);
-
-		if(!canBuild && this.parentId != null && !this.parentId.getNamespace().equals(advancementId.getNamespace())) {
-			SparkweaveLogging.getLogger().info("Unable to verify parent advancement {} for {}, forcing build to continue anyways.", this.parentId, advancementId);
-			return true;
-		}
-
-		return canBuild;
+	/**
+	 * vanilla always returns null here, making it impossible to set advancement parents via ID instead of directly.
+	 * We bypass this and log a warning instead, because we cannot resolve the actual advancement at this time.
+	 */
+	@ModifyReturnValue(method = "method_702", at = @At("RETURN"))
+	private static Advancement canActuallyBuild(Advancement _alwaysNull, ResourceLocation advancementId) {
+		SparkweaveLogging.getLogger().info("Unable to verify parent advancement {}, forcing build to continue anyways.", advancementId);
+		return new Advancement(advancementId, null, null, AdvancementRewards.EMPTY, Map.of(), RequirementsStrategy.AND.createRequirements(Set.of()), false);
 	}
 
 }
