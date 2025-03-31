75ba974b2d2686e3191b51de037350800bfab6c0
Up <upcraftedlp@gmail.com>
2025-04-01 01:52:26 +0200

    change fabric entity tick handling (fixes #3)

--- Fabric/src/main/java/dev/upcraft/sparkweave/fabric/mixin/ServerLevelMixin.java
+++ Fabric/src/main/java/dev/upcraft/sparkweave/fabric/mixin/ServerLevelMixin.java
@@ -1,8 +1,7 @@
 package dev.upcraft.sparkweave.fabric.mixin;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import com.llamalad7.mixinextras.sugar.Share;
-import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.upcraft.sparkweave.event.EntityTickEventsImpl;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
@@ -15,26 +14,22 @@ import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.storage.WritableLevelData;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Supplier;
 
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin extends Level {
-	protected ServerLevelMixin(WritableLevelData $$0, ResourceKey<Level> $$1, RegistryAccess $$2, Holder<DimensionType> $$3, Supplier<ProfilerFiller> $$4, boolean $$5, boolean $$6, long $$7, int $$8) {
-		super($$0, $$1, $$2, $$3, $$4, $$5, $$6, $$7, $$8);
+	private ServerLevelMixin(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, Supplier<ProfilerFiller> profiler, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates) {
+		super(levelData, dimension, registryAccess, dimensionTypeRegistration, profiler, isClientSide, isDebug, biomeZoomSeed, maxChainedNeighborUpdates);
 	}
 
-	@WrapWithCondition(method = "tickNonPassenger", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;tick()V"))
-	private boolean startOfTick(Entity entity, @Share("entity") LocalRef<Entity> ref) {
-		ref.set(entity);
-		return !EntityTickEventsImpl.getStartHandler(entity.getClass()).invoker().startTick(entity, this);
-	}
+	@WrapOperation(method = "tickNonPassenger", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;tick()V"))
+	private void onEntityTick(Entity instance, Operation<Void> original) {
+		if(EntityTickEventsImpl.getStartHandler(instance.getClass()).invoker().startTick(instance, this)) {
+			return;
+		}
 
-	@Inject(method = "tickNonPassenger", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;tick()V", shift = At.Shift.AFTER))
-	private void endOfTick(CallbackInfo info, @Share("entity") LocalRef<Entity> ref) {
-		Entity entity = ref.get();
-		EntityTickEventsImpl.getEndHandler(entity.getClass()).invoker().endTick(entity, this);
+		original.call(instance);
+		EntityTickEventsImpl.getEndHandler(instance.getClass()).invoker().endTick(instance, this);
 	}
 }
