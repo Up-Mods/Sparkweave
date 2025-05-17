773fb3ecb29749b4b861942d209a9a7ff105d555
Up <upcraftedlp@gmail.com>
2025-05-17 22:57:31 +0200

    refactor RenderDocHelper

--- Common/src/main/java/dev/upcraft/sparkweave/client/render/RenderDocHelper.java
+++ Common/src/main/java/dev/upcraft/sparkweave/client/render/RenderDocHelper.java
@@ -1,22 +1,20 @@
 package dev.upcraft.sparkweave.client.render;
 
-import dev.upcraft.sparkweave.SparkweaveMod;
-import dev.upcraft.sparkweave.api.SparkweaveApi;
-import dev.upcraft.sparkweave.api.annotation.Mod;
-import dev.upcraft.sparkweave.logging.SparkweaveLogging;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Mod.Context(SparkweaveMod.MODID)
 public class RenderDocHelper {
 
-	private static final Logger LOGGER = SparkweaveLogging.getLogger();
+	private static final Logger LOGGER = LoggerFactory.getLogger(RenderDocHelper.class);
+	public static final boolean LOAD_RENDERDOC = Boolean.getBoolean("sparkweave.debug.render.load_renderdoc");
 
 	public static void init() {
-		if(SparkweaveApi.Client.LOAD_RENDERDOC) {
+		if(LOAD_RENDERDOC) {
+			LOGGER.info("Loading RenderDoc");
 			var libraryPath = System.getProperty("java.library.path");
 
 			// if set, search in RENDERDOC_HOME first, then fall back to library path
