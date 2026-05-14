ed705c74116057413650a15db1dde2646269fb9b
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-05-14 11:06:38 +0200

    update to gradle 9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-
 rootProject.name = "Sparkweave"
 listOf("Common", "Fabric", "NeoForge").forEach {
     include(it)
