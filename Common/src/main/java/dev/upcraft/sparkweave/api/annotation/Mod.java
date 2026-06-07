0831f275fe3d1d77cdc5d9f7f4a77ea6d13557f3
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-06-07 11:42:38 +0200

    ⚰️ remove unused Mod.Entrypoint annotation

--- Common/src/main/java/dev/upcraft/sparkweave/api/annotation/Mod.java
+++ Common/src/main/java/dev/upcraft/sparkweave/api/annotation/Mod.java
@@ -5,7 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-public class Mod {
+public final class Mod {
+
+	private Mod() {
+		throw new UnsupportedOperationException();
+	}
 
 	@Target(value = {ElementType.TYPE, ElementType.PACKAGE})
 	@Retention(RetentionPolicy.RUNTIME)
@@ -15,19 +19,4 @@ public class Mod {
 		 */
 		String value();
 	}
-
-	// TODO see if this is needed at all, and find a way to make it work for multiple loader setups
-//	/**
-//	 * Marks an interface as being an entrypoint, which will cause the annotation processor
-//	 * to include it in the quilt.mod.json {@code entrypoints} section.
-//	 */
-//	@Target(value = ElementType.TYPE)
-//	@Retention(RetentionPolicy.RUNTIME)
-//	public @interface Entrypoint {
-//
-//		/**
-//		 * @return the id of the entrypoint
-//		 */
-//		String value();
-//	}
 }
