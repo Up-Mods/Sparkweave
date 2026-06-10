126a5e607f60e53e1c36d7e1e61f21c89347e071
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-06-10 23:52:13 +0200

    🎨 clean up manifest timestamp task

--- build-logic/src/main/kotlin/dev/upcraft/gradle/multiloader/multiloader-common.gradle.kts
+++ build-logic/src/main/kotlin/dev/upcraft/gradle/multiloader/multiloader-common.gradle.kts
@@ -139,7 +139,7 @@ tasks.named<Jar>("jar").configure {
         rename("LICENSE.md", "LICENSE_${rootProject.name}.md")
     }
 
-    manifest.from(tmpManifest.get().manifestPath.get())
+    manifest.from(tmpManifest.flatMap { it.manifestPath })
     dependsOn(tmpManifest)
 
     manifest.attributes(
