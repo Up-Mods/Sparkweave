9e4417ef011cbfd17c30c5da2d67ca14e5c30276
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-07-13 11:21:04 +0200

    🐛 fix test lectern renderer

--- Common/src/testmod/java/dev/upcraft/sparkweave/testmod/client/renderers/DiamondLecternRenderer.java
+++ Common/src/testmod/java/dev/upcraft/sparkweave/testmod/client/renderers/DiamondLecternRenderer.java
@@ -1,7 +1,6 @@
 package dev.upcraft.sparkweave.testmod.client.renderers;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 import dev.upcraft.sparkweave.api.client.render.LecternItemRenderer;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
@@ -33,12 +32,11 @@ public class DiamondLecternRenderer extends LecternItemRenderer<DiamondLecternRe
 	@Override
 	public void extractRenderState(LecternBlockEntity blockEntity, LecternRenderState baseState, RenderState customState, float partialTicks, Vec3 cameraPosition, ModelFeatureRenderer.@Nullable CrumblingOverlay breakProgress) {
 		var seed = (int) blockEntity.getBlockPos().asLong();
-		itemModelResolver.updateForTopItem(customState.stack, blockEntity.getBook(), ItemDisplayContext.ON_SHELF, blockEntity.getLevel(), blockEntity.sparkweave$asItemOwner(), seed);
+		itemModelResolver.updateForTopItem(customState.stack, blockEntity.getBook(), ItemDisplayContext.FIXED, blockEntity.getLevel(), blockEntity.sparkweave$asItemOwner(), seed);
 	}
 
 	@Override
 	public void submit(LecternRenderState baseState, RenderState customState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState camera) {
-		poseStack.mulPose(Axis.XN.rotationDegrees(67.5f));
 		poseStack.scale(0.5F, 0.5F, 0.5F);
 		customState.stack.submit(poseStack, submitNodeCollector, baseState.lightCoords, OverlayTexture.NO_OVERLAY, 0);
 	}
