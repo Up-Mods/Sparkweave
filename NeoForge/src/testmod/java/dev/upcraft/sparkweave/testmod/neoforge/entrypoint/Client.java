06610be072b03dcfe459dac830c7982ccf6de7cb
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-07-13 11:13:09 +0200

    ⚰️ remove unused testmod per-loader entrypoints

--- NeoForge/src/testmod/java/dev/upcraft/sparkweave/testmod/neoforge/entrypoint/Client.java
+++
@@ -1,6 +0,0 @@
-package dev.upcraft.sparkweave.testmod.neoforge.entrypoint;
-
-//@EventBusSubscriber(modid = SparkweaveTestmod.MODID, value = Dist.CLIENT)
-public class Client {
-
-}
--- NeoForge/src/testmod/java/dev/upcraft/sparkweave/testmod/neoforge/package-info.java
+++
@@ -1,5 +0,0 @@
-@Mod.Context(SparkweaveTestmod.MODID)
-package dev.upcraft.sparkweave.testmod.neoforge;
-
-import dev.upcraft.sparkweave.api.annotation.Mod;
-import dev.upcraft.sparkweave.testmod.SparkweaveTestmod;
