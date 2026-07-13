e4da239f89c3b8160349bec7709f4beb929c8537
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-07-13 11:20:43 +0200

    🔊 log entrypoint executions

--- Common/src/main/java/dev/upcraft/sparkweave/entrypoint/EntrypointHelper.java
+++ Common/src/main/java/dev/upcraft/sparkweave/entrypoint/EntrypointHelper.java
@@ -1,16 +1,22 @@
 package dev.upcraft.sparkweave.entrypoint;
 
+import dev.upcraft.sparkweave.api.logging.SparkweaveLoggerFactory;
 import dev.upcraft.sparkweave.api.platform.ModContainer;
 import dev.upcraft.sparkweave.api.platform.PlatformUtils;
 import dev.upcraft.sparkweave.api.reflect.ContextHelper;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ServiceLoader;
 import java.util.function.BiConsumer;
 
 public class EntrypointHelper {
 
+	private static final Logger LOGGER = SparkweaveLoggerFactory.getLogger();
+
 	public static <T> void fireEntrypoints(Class<T> clazz, BiConsumer<T, ModContainer> consumer, boolean validate) {
-		ServiceLoader.load(clazz).forEach(instance -> {
+		LOGGER.debug("Firing entrypoint {}", clazz.getName());
+		ServiceLoader.load(clazz, EntrypointHelper.class.getClassLoader()).forEach(instance -> {
+			LOGGER.debug("Entrypoint {}: Found candidate {}", clazz.getName(), instance.getClass().getName());
 			ModContainer modContainer = ContextHelper.getContext(instance.getClass());
 			if(modContainer == null) {
 				throw new IllegalStateException("Entrypoint instance " + instance.getClass().getName() + " does not have a mod container");
