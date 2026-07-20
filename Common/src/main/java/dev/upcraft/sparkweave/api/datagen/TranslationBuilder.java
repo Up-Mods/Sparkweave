81c59ea87bf2d296f2e1ba7bbe1bddf3551642c7
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-07-21 01:13:52 +0200

    ✨ add more translations datagen helpers

--- Common/src/main/java/dev/upcraft/sparkweave/api/datagen/TranslationBuilder.java
+++ Common/src/main/java/dev/upcraft/sparkweave/api/datagen/TranslationBuilder.java
@@ -4,9 +4,11 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import dev.upcraft.sparkweave.api.registry.RegistrySupplier;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.Util;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -29,20 +31,36 @@ public class TranslationBuilder {
 		mapBuilder.put(key, translation);
 	}
 
-	public void block(Supplier<? extends Block> block, String name) {
-		add(block.get().getDescriptionId(), name);
+	public void attribute(Supplier<? extends Attribute> attribute, String translation) {
+		add(attribute.get().getDescriptionId(), translation);
 	}
 
-	public void creativeTab(RegistrySupplier<CreativeModeTab> tab, String name) {
-		add(Util.makeDescriptionId("itemGroup", tab.getId()), name);
+	public void block(Supplier<? extends Block> block, String translation) {
+		add(block.get().getDescriptionId(), translation);
 	}
 
-	public void entity(RegistrySupplier<? extends EntityType<? extends Entity>> entity, String name) {
-		add(entity.get().getDescriptionId(), name);
+	public void creativeTab(RegistrySupplier<CreativeModeTab> tab, String translation) {
+		add(Util.makeDescriptionId("itemGroup", tab.getId()), translation);
 	}
 
-	public void item(Supplier<? extends Item> item, String name) {
-		add(item.get().getDescriptionId(), name);
+	public void entity(Supplier<? extends EntityType<? extends Entity>> entity, String translation) {
+		add(entity.get().getDescriptionId(), translation);
+	}
+
+	public void item(Supplier<? extends Item> item, String translation) {
+		add(item.get().getDescriptionId(), translation);
+	}
+
+	public void mobEffect(Supplier<? extends Block> effect, String translation) {
+		add(effect.get().getDescriptionId(), translation);
+	}
+
+	/**
+	 * should not be used in en_us file
+	 */
+	@ApiStatus.Experimental
+	public void tag(TagKey<?> tagKey, String translation) {
+		add(tagKey.getDescriptionId(), translation);
 	}
 
 	@ApiStatus.Internal
