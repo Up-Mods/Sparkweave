bcf5d626e360dbc8cd12561772792971151c6449
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-08-30 09:18:05 +0200

    🎨 rearrange field order to match blocks class

--- Common/src/testmod/java/dev/upcraft/sparkweave/testmod/init/TestItems.java
+++ Common/src/testmod/java/dev/upcraft/sparkweave/testmod/init/TestItems.java
@@ -21,8 +21,6 @@ public class TestItems {
 	public static final RegistrySupplier<Item> TEST_ITEM = ITEMS.register("test_item", Item::new, new Item.Properties());
 	public static final Holder<Item> TEST_ITEM_HOLDER_EARLY = TEST_ITEM.holder();
 
-	public static final Supplier<Item> BLUEBERRY = () -> TestBlocks.BLUEBERRY_BUSH.get().asItem();
-
 	public static final RegistrySupplier<Item> MAGE_HOOD = ITEMS.register("mage_hood", Item::new, () -> new Item.Properties().humanoidArmor(TestArmorMaterials.MAGE_ROBES, ArmorType.HELMET).component(TestDataComponents.OPENABLE.get(), Openable.OPEN));
 	public static final RegistrySupplier<Item> MAGE_ROBES = ITEMS.register("mage_robes", Item::new, () -> new Item.Properties().humanoidArmor(TestArmorMaterials.MAGE_ROBES, ArmorType.CHESTPLATE));
 	public static final RegistrySupplier<Item> MAGE_LEGGINGS = ITEMS.register("mage_leggings", Item::new, () -> new Item.Properties().humanoidArmor(TestArmorMaterials.MAGE_ROBES, ArmorType.LEGGINGS));
@@ -30,4 +28,6 @@ public class TestItems {
 
 	public static final RegistrySupplier<Item> TEST_SIGN = ITEMS.registerForBlock(TestBlocks.TEST_SIGN, properties -> new SignItem(TestBlocks.TEST_SIGN.get(), TestBlocks.TEST_WALL_SIGN.get(), properties), new Item.Properties().stacksTo(16));
 	public static final RegistrySupplier<Item> TEST_HANGING_SIGN = ITEMS.registerForBlock(TestBlocks.TEST_HANGING_SIGN, properties -> new HangingSignItem(TestBlocks.TEST_HANGING_SIGN.get(), TestBlocks.TEST_WALL_HANGING_SIGN.get(), properties), new Item.Properties().stacksTo(16));
+
+	public static final Supplier<Item> BLUEBERRY = () -> TestBlocks.BLUEBERRY_BUSH.get().asItem();
 }
