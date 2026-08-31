cb3275ef17abc75845a44b98959011dbcb127300
Up <10714589+UpcraftLP@users.noreply.github.com>
2026-08-31 10:35:05 +0200

    ✨ add more builtin types for translation builder

--- Common/src/main/java/dev/upcraft/sparkweave/api/datagen/TranslationBuilder.java
+++ Common/src/main/java/dev/upcraft/sparkweave/api/datagen/TranslationBuilder.java
@@ -4,16 +4,28 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import dev.upcraft.sparkweave.api.registry.RegistrySupplier;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.advancements.packs.VanillaAdventureAdvancements;
+import net.minecraft.data.advancements.packs.VanillaStoryAdvancements;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.Util;
+import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.damagesource.DeathMessageType;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.ApiStatus;
+import org.jspecify.annotations.Nullable;
 
 import java.util.Map;
 import java.util.function.Supplier;
@@ -32,13 +44,23 @@ public class TranslationBuilder {
 		mapBuilder.put(key, translation);
 	}
 
+	@Deprecated(forRemoval = true)
+	public void advancement(TranslationBuilder builder, Identifier advancementId, String title, String description) {
+		builder.add(Util.makeDescriptionId("advancements", advancementId.withSuffix(".title")), title);
+		builder.add(Util.makeDescriptionId("advancements", advancementId.withSuffix(".description")), description);
+	}
+
 	public void attribute(Supplier<? extends Attribute> attribute, String translation) {
 		add(attribute.get().getDescriptionId(), translation);
 	}
 
+	@ApiStatus.Experimental
+	public void biome(ResourceKey<Biome> biome, String translation) {
+		add(Util.makeDescriptionId("biome", biome.identifier()), translation);
+	}
+
 	public void block(Supplier<? extends Block> block, String translation) {
-		var blockId = block.get().getDescriptionId();
-		add(blockId, translation);
+		add(block.get().getDescriptionId(), translation);
 	}
 
 	public void creativeTab(RegistrySupplier<CreativeModeTab> tab, String translation) {
@@ -53,8 +75,38 @@ public class TranslationBuilder {
 		add(item.get().asItem().getDescriptionId(), translation);
 	}
 
+	@ApiStatus.Experimental
+	@SuppressWarnings("deprecation")
+	public void itemStack(TranslationBuilder builder, ItemStack stack, String translation) {
+		var itemName = stack.getItemName();
+		if(!(itemName.getContents() instanceof TranslatableContents translatableContents)) {
+			throw new IllegalArgumentException("Item name not translatable for [%s]: %s".formatted(stack.getItem().builtInRegistryHolder().key().identifier(), itemName));
+		}
+
+		var translationKey = translatableContents.getKey();
+		if(translationKey.equals(stack.getItem().getDescriptionId())) {
+			throw new IllegalArgumentException("Item translation %s is the default key, use item()/block() instead!".formatted(translationKey));
+		}
+
+		builder.add(translatableContents.getKey(), translation);
+	}
+
 	public void mobEffect(Supplier<? extends MobEffect> effect, String translation) {
 		add(effect.get().getDescriptionId(), translation);
+	}
+
+	public void damageType(ResourceKey<DamageType> typeKey, String defaultTranslation, @Nullable String killedByTranslation, @Nullable String killedWithItemTranslation) {
+		var damageTypes = registries.lookupOrThrow(Registries.DAMAGE_TYPE);
+		var type = damageTypes.getOrThrow(typeKey).value();
+
+		if(type.deathMessageType() != DeathMessageType.DEFAULT) {
+			throw new IllegalArgumentException("Death message type not currently supported: " + type.deathMessageType());
+		}
+
+		var translationKey = "death.attack." + type.msgId();
+		add(translationKey, defaultTranslation);
+		add(translationKey + ".player", killedByTranslation != null ? killedByTranslation : defaultTranslation);
+		add(translationKey + ".stack", killedWithItemTranslation != null ? killedWithItemTranslation : defaultTranslation);
 	}
 
 	/**
@@ -68,5 +120,9 @@ public class TranslationBuilder {
 	@ApiStatus.Internal
 	public Map<String, String> build() {
 		return mapBuilder.buildOrThrow();
+	}
+
+	public <T> HolderLookup.RegistryLookup<T> lookupOrThrow(ResourceKey<? extends Registry<? extends T>> registry) {
+		return registries.lookupOrThrow(registry);
 	}
 }
